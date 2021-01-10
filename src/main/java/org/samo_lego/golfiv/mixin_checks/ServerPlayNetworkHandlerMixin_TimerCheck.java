33eec6eec0aacc19d9d25f69854b6372680df99a
samolego <34912839+samolego@users.noreply.github.com>
2021-01-10 15:36:47 +0100

    Redoing timer check

--- src/main/java/org/samo_lego/golfiv/mixin_checks/ServerPlayNetworkHandlerMixin_TimerCheck.java
+++ src/main/java/org/samo_lego/golfiv/mixin_checks/ServerPlayNetworkHandlerMixin_TimerCheck.java
@@ -1,10 +1,12 @@
 package org.samo_lego.golfiv.mixin_checks;
 
-import net.minecraft.network.packet.c2s.play.KeepAliveC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import org.samo_lego.golfiv.casts.Golfer;
+import org.samo_lego.golfiv.mixin_checks.accessors.PlayerMoveC2SPacketAccessor;
+import org.samo_lego.golfiv.utils.CheatType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -14,8 +16,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Set;
 
+import static org.samo_lego.golfiv.GolfIV.golfConfig;
+import static org.samo_lego.golfiv.utils.CheatType.TIMER;
+
 @Mixin(ServerPlayNetworkHandler.class)
-public abstract class ServerPlayNetworkHandlerMixin_timerCheck {
+public abstract class ServerPlayNetworkHandlerMixin_TimerCheck {
 
     @Shadow public ServerPlayerEntity player;
     @Unique
@@ -37,25 +42,26 @@ public abstract class ServerPlayNetworkHandlerMixin_timerCheck {
             )
     )
     private void checkTimer(PlayerMoveC2SPacket packet, CallbackInfo ci) {
-        /*if(golfConfig.main.antiTimer) {
-            long currentPacketTime = System.currentTimeMillis();
-            long lastTime = this.lastPacketTime;
-            this.lastPacketTime = currentPacketTime;
+        if(golfConfig.main.antiTimer) {
+            if(packet instanceof PlayerMoveC2SPacket.Both || packet instanceof PlayerMoveC2SPacket.LookOnly || packet.getX(player.getX()) != player.getX() || packet.getY(player.getY()) != player.getY() || packet.getZ(player.getZ()) != player.getZ()) {
+                long currentPacketTime = System.currentTimeMillis();
+                long lastTime = this.lastPacketTime;
+                this.lastPacketTime = currentPacketTime;
 
+                if(lastTime != 0) {
+                    this.packetRate += (50 + lastTime - currentPacketTime);
+                }
 
-            if(lastTime != 0) {
-                this.packetRate += (50 + lastTime - currentPacketTime);
+                if(this.packetRate > 20) {
+                    ((Golfer) player).report(TIMER, 10);
+                    this.packetRate = 0;
+                }
             }
-
-            if(this.packetRate < -100) {
-                this.packetRate = -50;
+            else {
+                this.packetRate = -100;
+                this.lastPacketTime = 0;
             }
-            else if(this.packetRate > 300) {
-                ((Golfer) player).report(TIMER);
-                this.packetRate = 0;
-            }
-            //System.out.println(packetRate);
-        }*/
+        }
     }
 
     /**
@@ -71,35 +77,6 @@ public abstract class ServerPlayNetworkHandlerMixin_timerCheck {
      */
     @Inject(method = "teleportRequest(DDDFFLjava/util/Set;)V", at = @At("HEAD"))
     private void timerRebalance(double x, double y, double z, float yaw, float pitch, Set<PlayerPositionLookS2CPacket.Flag> set, CallbackInfo ci) {
-        //this.packetRate -= 50;
-    }
-
-    /**
-     * Tries to detect timer hack.
-     *
-     * @param packet
-     * @param ci
-     */
-    @Inject(
-            method = "onKeepAlive(Lnet/minecraft/network/packet/c2s/play/KeepAliveC2SPacket;)V",
-            at = @At("HEAD")
-    )
-    private void checkTimerWithKeepAlive(KeepAliveC2SPacket packet, CallbackInfo ci) {
-        /*if(golfConfig.main.antiTimer) {
-            long currentPacketTime = System.currentTimeMillis();
-            long lastTime = this.lastPacketTime;
-            this.lastPacketTime = currentPacketTime;
-
-
-            if(lastTime != 0) {
-                this.packetRate += (15000 + lastTime - currentPacketTime);
-            }
-
-            if(this.packetRate > 300) {
-                ((Golfer) player).report(TIMER);
-                this.packetRate = 0;
-            }
-            System.out.println(packetRate);
-        }*/
+        this.packetRate -= 50;
     }
 }
