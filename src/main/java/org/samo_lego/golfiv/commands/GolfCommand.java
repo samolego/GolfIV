d40a2307438de3f0e240776ecad3138f3f90a41d
samolego <34912839+samolego@users.noreply.github.com>
2021-01-20 13:49:44 +0100

    Fixes #21

--- src/main/java/org/samo_lego/golfiv/commands/GolfCommand.java
+++ src/main/java/org/samo_lego/golfiv/commands/GolfCommand.java
@@ -71,6 +71,8 @@ public class GolfCommand {
         for(ServerPlayerEntity player : targets) {
 
             ListTag cheatLog  = ((Golfer) player).getCheatLog();
+            if(cheatLog == null)
+                continue;
             StringBuilder cheats = new StringBuilder();
 
             if(cheatLog.size() > 0) {
