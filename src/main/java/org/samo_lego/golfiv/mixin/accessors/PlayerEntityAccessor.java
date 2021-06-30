d155cec494ee85563a7021a4e0e399dfda1cd7c0
Ampflower <ampflower@outlook.com>
2021-06-30 00:52:25 -0700

    Fix EntityTrackerDataPatch removing the wrong tracked values. Iron Golems and Withers also now have health aligned to every quarter.

---
+++ src/main/java/org/samo_lego/golfiv/mixin/accessors/PlayerEntityAccessor.java
@@ -0,0 +1,18 @@
+package org.samo_lego.golfiv.mixin.accessors;// Created 2021-30-06T00:10:45
+
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * @author Ampflower
+ * @since ${version}
+ **/
+@Mixin(PlayerEntity.class)
+public interface PlayerEntityAccessor {
+    @Accessor("ABSORPTION_AMOUNT")
+    static TrackedData<Float> getAbsorption() {
+        throw new AssertionError("Accessor failed.");
+    }
+}
