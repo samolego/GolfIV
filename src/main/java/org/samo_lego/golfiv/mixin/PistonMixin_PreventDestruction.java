5ca5312473af5f15e416727891899786700192af
Ampflower <ampflower@outlook.com>
2022-05-06 23:55:27 -0500

    Allow certain blocks to be destructible by pistons This allows for an admin to allow bedrock breaking without any of the other side effects of outright disabling it.

--- src/main/java/org/samo_lego/golfiv/mixin/PistonMixin_PreventDestruction.java
+++ src/main/java/org/samo_lego/golfiv/mixin/PistonMixin_PreventDestruction.java
@@ -1,10 +1,12 @@
 package org.samo_lego.golfiv.mixin;// Created 2021-08-06T02:20:50
 
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.PistonBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -19,8 +21,13 @@ import static org.samo_lego.golfiv.GolfIV.golfConfig;
 public class PistonMixin_PreventDestruction {
     @Redirect(method = "onSyncedBlockEvent", require = 0, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;removeBlock(Lnet/minecraft/util/math/BlockPos;Z)Z"))
     private boolean redirectWorld(World world, BlockPos pos, boolean move) {
-        if(!golfConfig.main.preventDestructionByHeadlessPistons || world.getBlockState(pos).isOf(Blocks.PISTON_HEAD)) {
+        if (!golfConfig.main.preventDestructionByHeadlessPistons || canRemove(world.getBlockState(pos).getBlock())) {
             return world.removeBlock(pos, move);
         } else return false;
+    }
+
+    @Unique
+    private boolean canRemove(Block block) {
+        return block == Blocks.PISTON_HEAD || golfConfig.main.allowedDestructibleByHeadlessPistons.contains(block);
     }
 }
